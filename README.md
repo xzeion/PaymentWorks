bee8f217899eb361e173234cc8239a5afa48966d
Brian <brian@pop-os.localdomain>
2022-08-06 23:40:22 -0400

    built out some css and tweaked overall layout

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PaymentWorks
 A simple coding test for PaymentWorks
+
+This coding exersize pulls code from the https://api-v3.mbta.com/ api that serves the greater boston area
+
+To build and run the project make sure to have docker and docker-compose installed on your system then run the following command:
+
+  docker-compose up --build
+
+Once the container is finished building open your browser and navigate to: **http://localhost:5000**
+
+###Things I would do if I spent more time:
+* build out dataclass's for all of the data stored with numerical values (eg. the various transport types)
+* if there were a larger scope I may have gone with a blueprint view instead of a function based one.
+* I would have used proper wtforms classes and data validation
+* there was a ton of data availabe in that api. You could expand with heaps of features.
+* Added a logger and pushed logs to an appropriate external provider
+* used pytest to make a testing suite. 
+* refined the CSS and and page layout.
+* depending on deploy stack, prop up a web server and some ssl certs
--- subway/app.py
+++ subway/app.py
@@ -1,49 +1,35 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from flask import Flask, Response, render_template, request
-import logging, os, requests, pdb
+import requests
 
 app = Flask(__name__)
 app.config['BUNDLE_ERRORS'] = True
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format = f'%(levelname)s: %(message)s' )
-log = app.logger.debug
-
 root_uri="http://api-v3.mbta.com/"
+
 
 def subway_routes():
     resp = requests.get(root_uri + 'routes')
+    #NOTE: type == 1 filters the data to subway routes only
     return [x for x in resp.json()['data'] if x['attributes']['type'] == 1] if resp.ok else []
+
 
 def stops(route_id):
     if not route_id:
         return []
     stops = requests.get(root_uri + f'stops?filter[route]={route_id}')
-    return [stop['attributes']['name'] for stop in stops.json()['data']] if stops.ok else []
+    return [(stop['attributes']['name'],stop['attributes']['address']) for stop in stops.json()['data']] if stops.ok else []
 
-def validate(routes,route):
+
+def validate(routes, route):
+    '''validates that the route returned to us from the page is one of the avaliable routes'''
     return route if route in routes else None
+
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    routes = subway_routes()
-    st = stops(validate([x['id'] for x in routes],request.values.get('line')))
-
     return render_template(
         'home.html',
-        data=routes,
+        data=subway_routes() if request.method == 'GET' else [],
         line=request.values.get('line',''),
-        stops=st or [],
+        stops=stops(validate([x['id'] for x in subway_routes()],request.values.get('line')))
     )
-    
-
-
-'''
-given more time I would:
-* build out key tables so data like id=1 would be better 
-represented as meaning 'subway lines' for example
-
-'''
